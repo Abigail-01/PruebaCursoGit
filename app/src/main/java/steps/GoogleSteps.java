814bb2a9f4c59a1030018cb8aebc9ac60ea24d93
Abigail Mosso <abigail.mrtz08@gmail.com>
2022-08-02 16:24:37 -0500

    setup inicial

---
+++ app/src/main/java/steps/GoogleSteps.java
@@ -0,0 +1,24 @@
+package steps;
+import io.cucumber.java.en.*;
+
+
+public class GoogleSteps {
+
+   @Given ("^I am on the Google search page$")
+   public void navigateToGoogle(){
+      
+   }
+   @When ("^I enter a search criteria$")
+   public void enterSearchCriteria(){
+      
+   }
+   @And ("^Clic on the search button$")
+   public void clickSearButton(){
+      
+   }
+   @Then ("^The results match the criteria$")
+   public void validateResults(){
+      
+   }
+
+}
