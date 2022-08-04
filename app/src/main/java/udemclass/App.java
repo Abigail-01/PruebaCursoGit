d96959e898237b52f4d4d3c3b75b25956f0d9de4
Abigail Mosso <abigail.mrtz08@gmail.com>
2022-08-04 01:24:29 -0500

    cambios en nuestra rama

--- app/src/main/java/udemclass/App.java
+++ app/src/main/java/udemclass/App.java
@@ -7,6 +7,7 @@ package udemclass;
 public class App {
     public String getGreeting() {
         return "::: Hello World GIRL ::: --------- ";
+        //estamos en rama prueba hacemos cambios
     }
 
     public static void main(String[] args) {
